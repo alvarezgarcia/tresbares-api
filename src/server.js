c5ca26d904bc73a80ee793d631bece4a03f2f117
Sebastián Alvarez <seba.alvarez16@gmail.com>
2018-08-06 01:23:25 -0300

    Agregado DELETE a cors

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
   next();
 });
